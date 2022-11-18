b3f9b4a148072b4b569308ecb5f48a26e78f07f1
MacBook <mac@MacBook-Air-MacBook.local>
2022-11-18 21:21:04 +0500

    basically timer works, but now it takes date from HTML. need to add a way to input date inside js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// tabs
 const tabheader = document.querySelector(".tabheader"),
     tabheaderItems = document.querySelectorAll(".tabheader__item"),
     tabcontentOptions = document.querySelectorAll(".tabcontent");
@@ -32,3 +33,46 @@ function showTabContent(index){
 }
 
 
+
+// timer
+
+const time = {
+    days: document.querySelector("#hours"),
+    hours: document.querySelector("#hours"),
+    minutes: document.querySelector("#minutes"),
+    seconds: document.querySelector("#seconds"),
+    timeSummary: 0,
+    calcTimeSummary(){
+        // calculates whole amount of
+        this.timeSummary = (+this.seconds.innerText * 1000) + (+this.minutes.innerText * 60 * 1000) + (+this.hours.innerText * 60 * 60 * 1000) + (+this.days.innerText * 60 * 60 * 1000 * 24);
+    },
+    setEndDay(futureDate){
+
+        this.timeSummary = (new Date()).prototype.getMilliseconds();
+
+    },
+    setTime(){
+        this.days.textContent = String( Math.trunc(this.timeSummary /  ( 60 * 60 * 1000 * 24)));
+        this.hours.textContent = String( Math.trunc(this.timeSummary /  ( 60 * 60 * 1000)) % 24);
+        this.minutes.textContent =  Math.trunc(this.timeSummary /  (60 * 1000) % 60);
+        this.seconds.textContent = String( Math.trunc(this.timeSummary /  1000) % 60);
+    },
+    changeTime(value){
+        this.timeSummary += value;
+    }
+}
+
+time.calcTimeSummary()
+const timerInterval = setInterval(()=>{
+    time.setTime();
+    time.changeTime(-1000);
+    if (time.timeSummary < 0){
+        clearInterval(timerInterval);
+    }
+}, 1000);
+
+
+
+
+
+// console.log(time.days.innerText, time.hours.innerText, time.minutes.innerText, time.seconds.innerText);
