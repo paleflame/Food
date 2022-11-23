c239dc68c29a551d7f8ecaa52bf9d3be3d47d7bd
Danil Vasilenko <mac@MacBook-Air-MacBook.local>
2022-11-23 16:56:57 +0500

    added time formating in timer. 09 replaces 9, 01 replaces 1 etc.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 // tabs
 const tabheader = document.querySelector(".tabheader"),
     tabheaderItems = document.querySelectorAll(".tabheader__item"),
@@ -38,47 +39,58 @@ function showTabContent(index){
 
 // timer
 
+
+function addZero(num){
+    if (num < 10){
+        return `0${num}`;
+    }
+    else {
+        return String(num);
+    }
+}
+const timer = document.querySelector(".promotion__timer");
 const time = {
     elements: {
-        days: document.querySelector("#days"),
-        hours: document.querySelector("#hours"),
-        minutes: document.querySelector("#minutes"),
-        seconds: document.querySelector("#seconds"),
+        days: timer.querySelector("#days"),
+        hours: timer.querySelector("#hours"),
+        minutes: timer.querySelector("#minutes"),
+        seconds: timer.querySelector("#seconds"),
     },
-
+    captions: {
+        days: timer.querySelector(".days-caption"),
+        hours: timer.querySelector(".hours-caption"),
+        minutes: timer.querySelector(".minutes-caption"),
+        seconds: timer.querySelector(".seconds-caption"),
+    },
     timeLeft: 0,
     setTimeLeft(date){
         this.timeLeft = new Date(date) - new Date().getTime();
     },
-    setTime(){
-        this.elements.days.textContent = String( Math.trunc(this.timeLeft /  ( 60 * 60 * 1000 * 24)));
-        this.elements.hours.textContent = String( Math.trunc(this.timeLeft /  ( 60 * 60 * 1000)) % 24);
-        this.elements.minutes.textContent =  String(Math.trunc(this.timeLeft /  (60 * 1000) % 60));
-        this.elements.seconds.textContent = String( Math.trunc(this.timeLeft /  1000) % 60);
+    updateTime(){
+        this.elements.days.textContent = addZero(Math.trunc(this.timeLeft /  ( 60 * 60 * 1000 * 24)));
+        this.elements.hours.textContent = addZero( Math.trunc(this.timeLeft /  ( 60 * 60 * 1000)) % 24);
+        this.elements.minutes.textContent =  addZero(Math.trunc(this.timeLeft /  (60 * 1000) % 60));
+        this.elements.seconds.textContent = addZero( Math.trunc(this.timeLeft /  1000) % 60);
     },
-    changeTimeCaption(){
+    changeCaptions(){
 
     },
     changeTime(value){
         this.timeLeft += value;
+        time.updateTime();
     }
 };
 
 
-time.setTimeLeft("2022-12-30");
+time.setTimeLeft("2022-10-30");
 console.log(time.timeLeft / (1000 * 60 * 60 * 24));
-time.setTime();
+time.updateTime();
+
 const timerInterval = setInterval(()=>{
-    time.setTime();
     time.changeTime(-1000);
-    if (time.timeSummary < 0){
+    if (time.timeLeft <= 0){
         clearInterval(timerInterval);
     }
 }, 1000);
-//
-
-
-
-
 
 // console.log(time.days.innerText, time.hours.innerText, time.minutes.innerText, time.seconds.innerText);
